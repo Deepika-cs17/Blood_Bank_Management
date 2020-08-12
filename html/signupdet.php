e984adbfcaf1dc020e90bb3d928613e8762eddb3
Deepika-cs17 <41473320+Deepika-cs17@users.noreply.github.com>
2020-08-12 09:12:25 +0530

    Add files via upload

---
+++ html/signupdet.php
@@ -0,0 +1,30 @@
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="../css/signup.css">
+<link rel="stylesheet" href="fonts/iconic/css/material-design-iconic-font.min.css">
+<title>Login Page</title>
+</head>
+<h1>LOGIN PAGE</h1>
+<center>
+<body>
+<div id="id1">
+<span class="login100-form-logo">
+	<i class="zmdi zmdi-hospital-alt"></i>
+</span>
+<h2 style="color:white; font-family:Algerian; font-size:200%;">SIGN UP</h2>
+<input type="varchar" class="text-line" name="username" placeholder="USER NAME" style="color:white; font-family:Berlin Sans FB; font-size:100%;" required>
+<span class="focus-input100" data-placeholder="&#xf207;"></span>
+<br>
+<input type="varchar" class="text-line" name="mail_id" placeholder="E-MAIL" style="color:white; font-family:Lucida Sans; font-size:100%;" required><br><br>
+<input type="varchar" class="text-line" name="mobile_no" placeholder="MOBILE NUMBER" style="color:white; font-family:Lucida Sans; font-size:100%;" required><br><br>
+<input type="password" class="text-line" name="password" placeholder="PASSWORD" style="color:white; font-family:Lucida Sans; font-size:100%;" required><br><br>
+<input type="password" class="text-line" name="con_password" placeholder="CONFIRM PASSWORD" style="color:white; font-family:Lucida Sans; font-size:100%;" required><br><br> 
+<div1 class="container-login100-form-btn">
+	<button class="login100-form-btn">
+			SIGN UP
+		</button>
+		</div1>
+</div>
+</form>
+</center>
+</body>
